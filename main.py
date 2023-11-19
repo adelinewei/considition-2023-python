9f00f1f2387734fd2547b2947ed73eca52de18f6
Adeline-Wei <adeline80916@gmail.com>
2023-11-19 14:36:25 +0100

    replace two f3100s with one f9100

--- main.py
+++ main.py
@@ -78,41 +78,7 @@ def main():
         if mapEntity and generalData:
             # ------------------------------------------------------------
             # ----------------Player Algorithm goes here------------------
-            solution = {LK.locations: {}}
-            if mapName not in [MN.sSandbox, MN.gSandbox]:
-                for key in mapEntity[LK.locations]:
-                    location = mapEntity[LK.locations][key]
-                    name = location[LK.locationName]
-
-                    salesVolume = location[LK.salesVolume]
-                    if salesVolume > 100:
-                        solution[LK.locations][name] = {
-                            LK.f9100Count: 1,
-                            LK.f3100Count: 0,
-                        }
-            else:
-                hotspot1 = mapEntity[HK.hotspots][0]
-                hotspot2 = mapEntity[HK.hotspots][1]
-
-                solution[LK.locations]["location1"] = {
-                    LK.f9100Count: 1,
-                    LK.f3100Count: 0,
-                    LK.locationType: generalData[GK.locationTypes][
-                        GK.groceryStoreLarge
-                    ][GK.type_],
-                    CK.longitude: hotspot1[CK.longitude],
-                    CK.latitude: hotspot1[CK.latitude],
-                }
-
-                solution[LK.locations]["location2"] = {
-                    LK.f9100Count: 0,
-                    LK.f3100Count: 1,
-                    LK.locationType: generalData[GK.locationTypes][GK.groceryStore][
-                        GK.type_
-                    ],
-                    CK.longitude: hotspot2[CK.longitude],
-                    CK.latitude: hotspot2[CK.latitude],
-                }
+            solution = try_(mapEntity, generalData, mapName)
             # ----------------End of player code--------------------------
             # ------------------------------------------------------------
 
@@ -142,6 +108,168 @@ def main():
                 print(f"id: {scoredSolution[SK.gameId]}")
                 print(f"Score: {scoredSolution[SK.gameScore]}")
 
+
+def try_(mapEntity, generalData, mapName):
+
+    solution = {LK.locations: {}}
+    not_in_solution = {LK.locations: {}}
+    if mapName not in [MN.sSandbox, MN.gSandbox]:
+
+        for key in mapEntity[LK.locations]:
+            loc = mapEntity[LK.locations][key]
+
+            # solution logic TODO
+            # TODO check relation between salse volumn and 1f9 capacity for each locqtions. 
+            # TODO check relation between local footfall and sales local volumn - > might effect the solution logic
+            # TODO if a location has little salse volume but a lot footfall, it's still a good locaton to set the f machines
+            # goal: salesCapacity close (>=?, depends on leasing cost) to sales volumn as much as possible
+            # maxima (salse) revenue, co2_saving total footfall for all selected locations
+            # minima leasing cost
+
+            distribute_scales = 0  # TODO add distributeScales, should be large
+            footfall = loc[LK.footfall]  # TODO total footfall for all selected locations should be large
+
+            sales_volume = (loc[LK.salesVolume] + distribute_scales) * generalData[GK.refillSalesFactor]
+            # sales_volume = loc[LK.salesVolume]
+
+            # dummy solution TODO replace it
+            # a x + b y = z
+            # a: f3_count = z - by / x
+            # b: f9_count, b < -(y - z) /y ?
+            # z: sales_volume
+            # x: f3_cap, y: f9_cap
+            # TODO should use sales_volume, not loc[LK.salesVolume]
+            if sales_volume < generalData[GK.f3100Data][GK.refillCapacityPerWeek]:
+                f3_count = 1
+                f9_count = 0
+            else:
+                f9_count = sales_volume // generalData[GK.f9100Data][GK.refillCapacityPerWeek]
+                f9_count = 2 if f9_count > 2 else f9_count
+                f3_count = (sales_volume - f9_count * generalData[GK.f9100Data][GK.refillCapacityPerWeek]) // generalData[GK.f3100Data][GK.refillCapacityPerWeek]
+                f3_count = 2 if f3_count > 2 else f3_count
+
+
+            if (f9_count < 2) and (f3_count == 2):
+                f3_count = 0
+                f9_count += 1
+                # Two f3s
+                # f3_sales_capacity = \
+                #     f3_count * generalData[GK.f3100Data][GK.refillCapacityPerWeek] * 2
+                # f3_sales = min(round(sales_volume, 0), f3_sales_capacity)
+                # f3_revenue = f3_sales * generalData[GK.refillUnitData][GK.profitPerUnit]
+                # f3_leasing_cost = \
+                #     generalData[GK.f3100Data][GK.leasingCostPerWeek] * 2
+                # f3_earnings = f3_revenue - f3_leasing_cost
+                # f3_co2_savings = f3_sales \
+                #     * (
+                #         generalData[GK.classicUnitData][GK.co2PerUnitInGrams]
+                #         - generalData[GK.refillUnitData][GK.co2PerUnitInGrams]
+                #     ) / 1000
+                # f3_co2_savings_rating = f3_co2_savings - \
+                #     2 * generalData[GK.f3100Data][GK.staticCo2] / 1000 \
+                #     - 1 * generalData[GK.f9100Data][GK.staticCo2] / 1000
+
+                # f3_co2_savings_price = f3_co2_savings_rating * generalData[GK.co2PricePerKiloInSek]
+                # f3_total = f3_co2_savings_price + f3_earnings
+
+                # # One f9
+                # f9_sales_capacity = \
+                #     f9_count * generalData[GK.f9100Data][GK.refillCapacityPerWeek]
+                # f9_sales = min(round(sales_volume, 0), f9_sales_capacity)
+                # f9_revenue = f9_sales * generalData[GK.refillUnitData][GK.profitPerUnit]
+                # f9_leasing_cost = \
+                #     generalData[GK.f9100Data][GK.leasingCostPerWeek]
+                # f9_earnings = f9_revenue - f9_leasing_cost
+                # f9_co2_savings = f9_sales \
+                #     * (
+                #         generalData[GK.classicUnitData][GK.co2PerUnitInGrams]
+                #         - generalData[GK.refillUnitData][GK.co2PerUnitInGrams]
+                #     ) / 1000
+                # f9_co2_savings_rating = f9_co2_savings - \
+                #     2 * generalData[GK.f3100Data][GK.staticCo2] / 1000 \
+                #     - 1 * generalData[GK.f9100Data][GK.staticCo2] / 1000
+                # f9_total = f9_co2_savings_rating + f9_earnings
+
+                # print(f'f9_earnings: {f9_earnings}')
+                # print(f'f3_total: {f3_total}')
+                # if f9_total > f3_total:
+                #     f3_count = 0
+                #     f9_count += 1
+
+
+            # validation
+            sales_capacity = \
+                f3_count * generalData[GK.f3100Data][GK.refillCapacityPerWeek] \
+                + f9_count * generalData[GK.f9100Data][GK.refillCapacityPerWeek]
+
+            sales = min(round(sales_volume, 0), sales_capacity)
+            revenue = sales * generalData[GK.refillUnitData][GK.profitPerUnit]
+            leasing_cost = \
+                f3_count * generalData[GK.f3100Data][GK.leasingCostPerWeek] \
+                + f9_count * generalData[GK.f9100Data][GK.leasingCostPerWeek]
+            earnings = revenue - leasing_cost
+            co2_savings = sales \
+                * (
+                    generalData[GK.classicUnitData][GK.co2PerUnitInGrams]
+                    - generalData[GK.refillUnitData][GK.co2PerUnitInGrams]
+                ) / 1000
+            co2_savings_rating = co2_savings - \
+                f3_count * generalData[GK.f3100Data][GK.staticCo2] / 1000 \
+                - f9_count * generalData[GK.f9100Data][GK.staticCo2] / 1000
+
+            co2_savings_price = co2_savings_rating * generalData[GK.co2PricePerKiloInSek]
+
+            local_score_exclude_footfall = co2_savings_price + earnings
+
+            if (f3_count + f9_count < 1) or (local_score_exclude_footfall < 0):
+
+                not_in_solution[key] = {
+                    LK.locationName: loc[LK.locationName],
+                    LK.locationType: loc[LK.locationType],
+                    CK.latitude: loc[CK.latitude],
+                    CK.longitude: loc[CK.longitude],
+                    LK.footfall: loc[LK.footfall],
+                    LK.salesVolume: loc[LK.salesVolume] * generalData[GK.refillSalesFactor],
+                }
+                continue
+
+            # print("key----------------------------", key)
+            # print(f"#f9: {f9_count}, #f3: {f3_count}")
+            # print("local_score_exclude_footfall:", local_score_exclude_footfall)
+            # print("co2_savings_price:", co2_savings_price)
+            # print("earnings", earnings)
+
+            # add to solution dict
+            solution[LK.locations][key] = {
+                LK.f9100Count: int(f9_count),
+                LK.f3100Count: int(f3_count),
+            }
+    else:
+        print("======== Sandbox ========")
+        hotspot1 = mapEntity[HK.hotspots][0]
+        hotspot2 = mapEntity[HK.hotspots][1]
+
+        solution[LK.locations]["location1"] = {
+            LK.f9100Count: 1,
+            LK.f3100Count: 0,
+            LK.locationType: generalData[GK.locationTypes][
+                GK.groceryStoreLarge
+            ][GK.type_],
+            CK.longitude: hotspot1[CK.longitude],
+            CK.latitude: hotspot1[CK.latitude],
+        }
+
+        solution[LK.locations]["location2"] = {
+            LK.f9100Count: 0,
+            LK.f3100Count: 1,
+            LK.locationType: generalData[GK.locationTypes][GK.groceryStore][
+                GK.type_
+            ],
+            CK.longitude: hotspot2[CK.longitude],
+            CK.latitude: hotspot2[CK.latitude],
+        }
+
+    return solution
 
 if __name__ == "__main__":
     main()
